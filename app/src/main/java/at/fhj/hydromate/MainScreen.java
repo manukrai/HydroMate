c178c378cc220aeedf25f6347cfeb2228db074ff
manuel.krainer <manuel.krainer@edu.fh-joanneum.at>
2025-05-12 22:07:40 +0200

    Add - daily Intake

--- app/src/main/java/at/fhj/hydromate/MainScreen.java
+++ app/src/main/java/at/fhj/hydromate/MainScreen.java
@@ -13,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 
+
+
 public class MainScreen extends AppCompatActivity {
 
     @Override
@@ -27,9 +29,18 @@ public class MainScreen extends AppCompatActivity {
         });
 
         TextView textMili = findViewById(R.id.tvMl);
+        TextView textProcent = findViewById(R.id.tvProcent);
 
-        textMili.setText(getIntent().getIntExtra("volume",0) + " ml");
+        int volume = getIntent().getIntExtra("volume",0);
+        double dailyIntake = getDailyIntake(86,185,22,"Male",15,5000);
 
+        textMili.setText(volume + " ml / " +(int)dailyIntake + " ml");
+
+        double procent = (volume/dailyIntake) * 100;
+        double roundedProcent = Math.round(procent * 100.0) / 100.0;
+
+
+        textProcent.setText(roundedProcent + " % of your Goal");
     }
 
     public void startDrinkScreen(View view) {
@@ -81,6 +92,42 @@ public class MainScreen extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public double getDailyIntake(int weight, int height,int age, String gender, int temperature, int steps)
+    {
+        double dailyIntake = (gender == "Male" ? 35 : 31) * weight;
+
+        // Alter berücksichtigen
+        if (age >= 65)
+        {
+            dailyIntake *= 0.9;
+        }
+
+        // Temperaturzuschlag
+        if (temperature >= 20 && temperature <= 25)
+        {
+            dailyIntake += 200;
+        }
+        else if (temperature > 25 && temperature <= 30)
+        {
+            dailyIntake += 500;
+        }
+        else if (temperature > 30)
+        {
+            dailyIntake += 700;
+        }
+
+        // Aktivitätszuschlag durch Schritte
+        if (steps >= 5000 && steps <= 10000)
+        {
+            dailyIntake += 300;
+        }
+        else if (steps > 10000)
+        {
+            dailyIntake += 500;
+        }
+
+        return dailyIntake;
+    }
 
 
 
--- app/src/main/java/at/fhj/hydromate/SettingScreen.java
+++ app/src/main/java/at/fhj/hydromate/SettingScreen.java
@@ -3,6 +3,8 @@ package at.fhj.hydromate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,6 +28,17 @@ public class SettingScreen extends AppCompatActivity {
 
     public void startMainScreen(View view) {
         Intent intent = new Intent(SettingScreen.this, MainScreen.class);
+
+        EditText etWeight = findViewById(R.id.etWeight);
+        EditText etHeight = findViewById(R.id.etHeight);
+        EditText etAge = findViewById(R.id.etAge);
+        RadioGroup rbGender = findViewById(R.id.rbGroupGender);
+
+        intent.putExtra("weight",etWeight.getText());
+        intent.putExtra("height",etHeight.getText());
+        intent.putExtra("age",etAge.getText());
+        intent.putExtra("gender","");
+
         startActivity(intent);
     }
 }
