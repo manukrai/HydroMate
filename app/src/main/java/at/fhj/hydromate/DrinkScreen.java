bd0e7995d62694b0edc6291956f7fb709a1d1ade
manuel.krainer <manuel.krainer@edu.fh-joanneum.at>
2025-05-12 21:36:40 +0200

    Add - drinkType - volume

--- app/src/main/java/at/fhj/hydromate/DrinkScreen.java
+++ app/src/main/java/at/fhj/hydromate/DrinkScreen.java
@@ -2,7 +2,10 @@ package at.fhj.hydromate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DrinkScreen extends AppCompatActivity {
+
+    private double drinkProcent = 0.00;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,71 @@ public class DrinkScreen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = getIntent();
+
+
+
+        ImageView imageView = findViewById(R.id.ivIcon);
+
+        switch(intent.getStringExtra("drinkType"))
+        {
+            case "water":imageView.setImageResource(R.drawable.water_bottle);
+                drinkProcent = 1.00;
+                break;
+            case "coffee":imageView.setImageResource(R.drawable.coffee_cup);
+                drinkProcent = 0.95;
+                break;
+            case "juice":imageView.setImageResource(R.drawable.juice);
+                drinkProcent = 0.90;
+                break;
+            case "tea":imageView.setImageResource(R.drawable.tea);
+                drinkProcent = 1.00;
+                break;
+            case "milk":imageView.setImageResource(R.drawable.milk);
+                drinkProcent = 1.15;
+                break;
+            case "beer":imageView.setImageResource(R.drawable.beer);
+                drinkProcent = 0.80;;
+                break;
+            case "strongAlcohol":imageView.setImageResource(R.drawable.liquor);
+                drinkProcent = 0.50;
+                break;
+        }
+
     }
 
     public void startMainScreen(View view) {
         Intent intent = new Intent(DrinkScreen.this, MainScreen.class);
+
+        intent.putExtra("drinkProcent",drinkProcent);
+
+        if(view.getId() == R.id.btSave)
+        {
+            EditText etVolume = findViewById(R.id.etVolume);
+            if(etVolume != null)
+            {
+                intent.putExtra("volume",etVolume.getText());
+            }
+            else
+            {
+                intent.putExtra("volume",0);
+            }
+
+        }
+        if(view.getId() == R.id.btAdd150)
+        {
+            intent.putExtra("volume",150);
+        }
+        if(view.getId() == R.id.btAdd250)
+        {
+            intent.putExtra("volume",250);
+        }
+        if(view.getId() == R.id.btadd500)
+        {
+            intent.putExtra("volume",500);
+        }
+
         startActivity(intent);
     }
 }
--- app/src/main/java/at/fhj/hydromate/MainScreen.java
+++ app/src/main/java/at/fhj/hydromate/MainScreen.java
@@ -2,7 +2,9 @@ package at.fhj.hydromate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,11 +26,54 @@ public class MainScreen extends AppCompatActivity {
             return insets;
         });
 
+        TextView textMili = findViewById(R.id.tvMl);
+
+        textMili.setText(getIntent().getIntExtra("volume",0) + " ml");
+
     }
 
     public void startDrinkScreen(View view) {
         Intent intent = new Intent(MainScreen.this, DrinkScreen.class);
+
+
+
+        if(view.getId() == R.id.btWater)
+        {
+            intent.putExtra("drinkType","water");
+        }
+
+        if(view.getId() == R.id.btCoffee)
+        {
+            intent.putExtra("drinkType","coffee");
+        }
+
+        if(view.getId() == R.id.btJuice)
+        {
+            intent.putExtra("drinkType","juice");
+        }
+
+        if(view.getId() == R.id.btTea)
+        {
+            intent.putExtra("drinkType","tea");
+        }
+
+        if(view.getId() == R.id.btMilk)
+        {
+            intent.putExtra("drinkType","milk");
+        }
+
+        if(view.getId() == R.id.btBeer)
+        {
+            intent.putExtra("drinkType","beer");
+        }
+
+        if(view.getId() == R.id.btStrongAlcohol)
+        {
+            intent.putExtra("drinkType","strongAlcohol");
+        }
+
         startActivity(intent);
+
     }
 
     public void startSettingsScreen(View view) {
